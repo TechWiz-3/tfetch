924567b008d9db338086b265fe8efa755db3fe9b
TechWiz-3 <programmingplus1@gmail.com>
2022-06-22 22:10:05 +1000

    👌 IMPROVE: naming, config file not present response

--- tfetch.py
+++ tfetch.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import distro
 import wmctrl
@@ -8,13 +10,13 @@ import json
 import time
 from subprocess import run
 
-def termRun(command, arguments):
+def term_run(command, arguments):
     output = subprocess.run([command, arguments],text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return output.stdout
 
-def getOS(architecture=False, removeLinux=False):
-    os = distro.linux_distribution()[0]
-    if removeLinux:
+def get_os(architecture=False, remove_linux=False):
+    os = distro.name()
+    if remove_linux:
         os = re.sub('linux', '', os, flags=re.IGNORECASE)
     os = os.rstrip()
     if architecture:
@@ -22,40 +24,40 @@ def getOS(architecture=False, removeLinux=False):
     os = os.lower()
     return os
 
-def getWM():
+def get_wm():
     try:
-        return wmctrl.os.environ.get('DESKTOP_SESSION').split("/")[-1].lower()
+        return wmctrl.os.environ.get('DESKTOP_SESSION').lower()
     except:
-        pass
+       pass 
     try:
         return wmctrl.os.environ.get('XDG_SESSION_DESKTOP')
     except:
         return None
 
-def getKernel(fullName=True):
+def get_kernel(full_name=True):
     kernel = platform.release()
-    if not fullName:
+    if not full_name:
         kernel = kernel.split('-')[0]
     return kernel
 
-def getPackages(displayPackageManager=False):
+def get_packages(display_package_manager=False):
     try:
-        packages = termRun('pacman', '-Qq')
+        packages = term_run('pacman', '-Qq')
         string = str(len(packages.split('\n')))
-        if displayPackageManager:
+        if display_package_manager:
             string += ' (pacman)'
         return string
     except:
         return None
 
 
-def checkColors(num: int):
+def check_colors(num: int):
     if num < 7:
         return True
     if num > 7 :
         return False
 
-def transColor(num : int):
+def trans_color(num: int):
     if num == 0:
         return '\033[30m'
     elif num == 1:
@@ -77,34 +79,33 @@ def transColor(num : int):
 
 
 
-def main(color1:str, color2:str):
+def main(color_a:str, color_b:str):
     bold = "\u001b[1m"
     reset = "\033[0m"
-    linuxos = getOS(removeLinux=True)
-    wm = getWM()
+    linux_os = get_os(remove_linux=True)
+    wm = get_wm()
     de = subprocess.Popen(['wmctrl -m | grep "Name"'], stdout=subprocess.PIPE,shell=True).communicate()[0]
     de = str(de, "utf-8").strip("\n").strip("Name:").replace(" ","").lower()
-    kernel = getKernel(fullName=False)
-    pkgs = getPackages(displayPackageManager=False)
+    kernel = get_kernel(full_name=False)
+    pkgs = get_packages(display_package_manager=False)
     shell = wmctrl.getoutput("echo $SHELL").replace("/","").replace("bin","")
 
     base = f""".
-{reset}├─ {bold}{color1}distro{reset}
-{reset}│  ├─ {color2}{linuxos}{reset}
-{reset}│  └─ {color2}{kernel}{reset}
-{reset}├─ {bold}{color1}pacman{reset}
-{reset}│  └─ {bold}{color2}packages{reset}
-{reset}│     └─ {color2}{pkgs}{reset}
-{reset}├─ {bold}{color1}env{reset}
-{reset}│  ├─ {bold}{color2}de{reset}
-{reset}│  │  └─ {color2}{de}{reset}
-{reset}│  └─ {bold}{color2}wm{reset}
-{reset}│     └─ {color2}{wm}{reset}
-{reset}└─ {bold}{color1}shell{reset}
-{reset}   └─ {color2}{shell}{reset}"""
+{reset}├─ {bold}{color_a}distro{reset}
+{reset}│  ├─ {color_b}{linux_os}{reset}
+{reset}│  └─ {color_b}{kernel}{reset}
+{reset}├─ {bold}{color_a}pacman{reset}
+{reset}│  └─ {bold}{color_a}packages{reset}
+{reset}│     └─ {color_b}{pkgs}{reset}
+{reset}├─ {bold}{color_a}env{reset}
+{reset}│  ├─ {bold}{color_b}de{reset}
+{reset}│  │  └─ {color_b}{de}{reset}
+{reset}│  └─ {bold}{color_b}wm{reset}
+{reset}│     └─ {color_b}{wm}{reset}
+{reset}└─ {bold}{color_a}shell{reset}
+{reset}   └─ {color_b}{shell}{reset}"""
 
     print(base)
-
 
 
 if __name__ == '__main__':
@@ -113,28 +114,33 @@ if __name__ == '__main__':
         with open('./config.json') as f:
             data = json.load(f)
     except:
-        input("Missing Config File")
+        print(
+            "Missing Config File (config.json).\n\n"
+            "You can link the full path to it on line 114 of tfetch\n"
+            "Example: /Users/Peregrine/Desktop/tfetch/config.json"
+        )
         exit()
 
     # Check the data
-    
-    color1check = checkColors(data['color1'])
-    color2check = checkColors(data['color2'])
+
+    color1check = check_colors(data['color1'])
+    color2check = check_colors(data['color2'])
 
     if color1check == True:
-        color1 = transColor(data['color1'])
+        color_a = trans_color(data['color1'])
     else:
-        color1 = ""
+        color_a = ""
 
     if color2check == True:
-        color2 = transColor(data['color2'])
+        color_b = trans_color(data['color2'])
     else:
-        color2 = ""
+        color_b = ""
 
     if color1check == True and color2check == True:
-        main(color1, color2)
+        main(color_a, color_b)
     else:
         print(" -> Using Default Config.")
         time.sleep(1)
         os.system("clear")
         main("\033[31m","\033[33m")
+
